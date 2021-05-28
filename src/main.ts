7f1f5bb1f18e463f2506758deb058ceda6bb8929
geone96 <geone.lemobs@gmail.com>
2021-05-28 17:30:40 -0300

    Adicionado config module, e variáveis de ambiente

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from './config/config.service';
+import validationExceptionFactory from './utils/validation/validation-exception.factory';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: validationExceptionFactory,
+      transform: true,
+      whitelist: true,
+    })
+  )
+
+  const config = new DocumentBuilder()
+  .setTitle('Star Wars API')
+  .setDescription('API com funcionalidades para tratamento dados do star wars')
+  .setVersion('1.0.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const CONFIGURACOES: ConfigService = app.get('ConfigService');
+
+  await app.listen(CONFIGURACOES.get('API_PORTA'));
+  Logger.log(`API rodando na porta ${CONFIGURACOES.get('API_PORTA')}`)
 }
 bootstrap();
